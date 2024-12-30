e390ed78238dbd8b1661eb5f1a1c3276d84ad760
devchitranshu <schitranshu17@gmail.com>
2024-12-30 17:07:26 +0530

    factorial with while loop

--- Beginner_Tasks/Exercise_2/factorial.py
+++ Beginner_Tasks/Exercise_2/factorial.py
@@ -9,4 +9,18 @@ else:
         factorial = factorial*i
     print("the factorial of a",nb,"is",factorial)
 
+# n = int(input("enter the number of which you want to get factorial : "))
+# fac = 1
+ # i = 1
+# while i <= n:
+#     fac *= i
+#      i += 1
+# print("the factorial is :"fac)
 
+
+# n = int(input("Enter the number you want to find the factorial of : "))
+# fac = 1
+
+# for i in range(1,n+1):
+# fac *= i
+#print=("the factorial is :", fac)
